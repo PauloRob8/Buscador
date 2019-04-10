1f4cdf92db71a2646b20f7ef435fb75dd01a2566
Paulo Roberto <paulo.rob159@gmail.com>
2019-04-10 16:22:50 -0300

    Adicionando buscador de palavras

--- Buscador.py
+++ Buscador.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def main():
 
@@ -7,17 +8,34 @@ def main():
     palavra = input("Digite a palavra:")
     profundidade = int(input("Digite a profundidade da busca:"))
 
-    response = requests.get(url)
-    html = BeautifulSoup(response.text,'lxml')
-    links = html.find_all(palavra)
+    #response = requests.get(url)
+    #html = BeautifulSoup(response.text ,'lxml')
+    #links = html.find_all('a')
 
-    for link in links:
-        if(str(link["href"]).startswith("http")):
-            print(link['href'])
+
+    #for link in links:
+        #if(str(link["href"]).startswith("http")):
+            #print(link['href'])
+
+    get_palavra(url,palavra)
 
 def get_palavra(url,palavra):
 
-    response = requests.get(url)
+    try:
+
+        response = requests.get(url)
+        text = BeautifulSoup(response.text, 'lxml').text
+        encontradas = re.findall('\w*.{0,11}' + palavra + '.{0,11}\w*', text, re.IGNORECASE)
+        for palavra in encontradas:
+            print('----' + palavra + '----')
+
+    except:
+        print("Link inválido")
+        pass
+
+def clear(text):
+
+    body = text.body
 
 
 if __name__ == '__main__':
